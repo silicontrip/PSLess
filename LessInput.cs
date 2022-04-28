aea8fe83a832dcc13ce21894ab0fb02447c7e61f
Mark (NGTV) Heath <silicontrip@gmail.com>
2022-04-28 15:45:19 +1000

    Adding more less commands

--- LessInput.cs
+++ LessInput.cs
@@ -8,10 +8,7 @@ namespace net.ninebroadcast {
 	{
 		protected LessController controller;
 		protected PSHostUserInterface hostui;
-
-		// public abstract void init (TextDocument td,LessDisplay ld);
-		public virtual LessInput beginParse () { return null; }
-		//public void init() { ; }
+		public virtual LessInput beginParse() { return null; }
 	}
 
 	public class ExitInput : LessInput {
@@ -32,7 +29,6 @@ namespace net.ninebroadcast {
 			return new DefaultInput(controller,"");
 		}
 	}
-
 	public class DefaultInput : LessInput {
 
 		private string numericInput;
@@ -63,25 +59,30 @@ namespace net.ninebroadcast {
 			
 			char key = ki.Character;
 			int code = ki.VirtualKeyCode;
+			ControlKeyStates control = ki.ControlKeyState;  // this returns no data
+
+			if (key!=0)
+				code = '\0';
+
 			// change state option
 
 			// check for control key and modify code appropriately
 			// if (ki.modifierFlags)
 
 
-			if (ki.Character == ':') {
+			if (key == ':') {
 				return new ColonInput(controller,numericInput);
 			}
-			if (ki.Character == 'q' || ki.Character == 'Q') 
+			if (key == 'q' || key == 'Q') 
 				return null;
 
-			if (ki.Character == 'Z')
+			if (key == 'Z')
 				return new ExitInput(controller,numericInput); // not sure if we should make the constructors the same.
 
-			if (ki.VirtualKeyCode==27)
+			if (code==27)
 				return new EscapeInput(controller,numericInput);
 
-			if (ki.Character=='=')  // or ^G :f
+			if (key=='=' || code=='g')  // or ^G :f
 			{
 				// less-help.txt lines 78-141/236 byte 7335/11925 61%  (press RETURN)
 				controller.displayCurrentFileDetails();
@@ -90,47 +91,107 @@ namespace net.ninebroadcast {
 
 // these commands take an optional numeric argument.
 // their behaviour changes when a number is involved.
+//  e  ^E  j  ^N  CR  *  Forward  one line   (or N lines).                                                             
 
-			if (ki.Character=='e' || 
-			    ki.Character=='j' || 
-				ki.VirtualKeyCode==13)
+			if (key=='e' || key=='j' || code==13 || code=='e' || code=='n')
 			{
 				//controller.resetStatus();
 				controller.oneLineForward(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  y  ^Y  k  ^K  ^P  *  Backward one line   (or N lines).                                                             
 
-			if (ki.Character=='y' ||
-			 ki.Character=='k') 
+			if (key=='y' || key=='k' || code=='y' || code=='k' || code == 'p')
 			{
 				//controller.resetStatus();
 				controller.oneLineBackward(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  f  ^F  ^V  SPACE  *  Forward  one window (or N lines).                                                             
 
-			if (ki.Character == ' ' ||
-				ki.Character == 'f' )
+			if (key == ' ' || key == 'f' || code=='f' || code=='v')
 			{
 				//controller.resetStatus();
 				controller.oneWindowForward(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  b  ^B  ESC-v      *  Backward one window (or N lines).                                                             
 
-			if (ki.Character == 'b') {
+			if (key == 'b' || code =='b') {
 				//controller.resetStatus();
 				controller.oneWindowBackward(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  z                 *  Forward  one window (and set window to N).                                                    
 
-			if (ki.Character=='z')
+			if (key=='z')
 			{
 				// reset numericInput 
 				// clear status Line
 				//  controller.resetStatus();
-				controller.setWindowHeight(numericInput);
+				controller.setWindowHeight(numericInput); // check this for sanitizing
 				controller.oneWindowForward(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  w                 *  Backward one window (and set window to N).                                                    
+
+			if (key=='w')
+			{
+				// reset numericInput 
+				// clear status Line
+				//  controller.resetStatus();
+				controller.setWindowHeight(numericInput); // check this for sanitizing
+				controller.oneWindowBackward("");
+				return new DefaultInput(controller,"");
+			}
+//   d  ^D             *  Forward  one half-window (and set half-window to N).                                          
+			if (key=='d' || code=='d')
+			{
+				controller.setWindowHalfHeight(numericInput); // half window?
+				controller.halfWindowForward();
+				return new DefaultInput(controller,"");
+			}
+//   u  ^U             *  Backward  one half-window (and set half-window to N).                                          
+
+			if (key=='u' || code=='u')
+			{
+				controller.setWindowHalfHeight(numericInput); // half window?
+				controller.halfWindowBackward();
+				return new DefaultInput(controller,"");
+			}
+
+//  ESC-)  RightArrow *  Right one half screen width (or N positions).                                                 
+			if (code==39)
+			{
+				controller.halfWindowRight(numericInput);
+				return new DefaultInput(controller,"");
+			}
+//  ESC-(  LeftArrow  *  Left one half screen width (or N positions).
+			if (code==37)
+			{
+				controller.halfWindowLeft(numericInput);
+				return new DefaultInput(controller,"");
+			}
+// Unable to check for ctrl-cursor
+//  ESC-}  ^RightArrow   Right to last column displayed.                                                                 
+//  ESC-{  ^LeftArrow    Left  to first column.
+//   F                    Forward forever; like "tail -f".                                                                
+
+			if (key=='F')
+			{
+				// controller.foreverForward();
+				;
+			}
+//  r  ^R  ^L            Repaint screen.                                                                                 
+//  R                    Repaint screen, discarding buffered input. 
+// RawUI cannot use screen buffer over PSRemoting:
+// System.Management.Automation.Host.BufferCell                                               
+	        if (key=='R' || key=='r' || code=='r' || code=='l')
+			{
+				controller.repaintScreen();
+			}
+
+controller.Status ( "" + key +"(" + code + ") / " +control);
 
 			controller.Alert();
 			return this;
@@ -152,41 +213,54 @@ namespace net.ninebroadcast {
 			if (ki.Character =='Q' || ki.Character == 'q')
 				return null;
 
+//  :e [file]            Examine a new file.
 			if (ki.Character == 'e')
 			{
 				//read filename
+				controller.Status("Examine: ");  // needed???
 				string filename = "";
-				while (ki.VirtualKeyCode != 13)
-				{
-					// is it a valid character
-					filename = filename + ki.Character;
-				}
+
+				LineInput readFileName = new LineInput(controller,"Examine: ");
+
+				readFileName.beginParse();
+
+				filename = readFileName.getLine();
+
 				// controller.examineFileNew(filename);
 				return new DefaultInput(controller,"");
 			}
+//  :n                *  Examine the (N-th) next file from the command line.
 			if (ki.Character == 'n')
 			{
 				// controller.resetStatus();
 				controller.examineFileNext(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  :p                *  Examine the (N-th) previous file from the command line.
 			if (ki.Character == 'p')
 			{
 				//controller.resetStatus();
 				controller.examineFilePrevious(numericInput);
 				return new DefaultInput(controller,"");
-
 			}
+//  :x                *  Examine the first (or N-th) file from the command line.
 			if (ki.Character == 'x')
 			{
 				//controller.resetStatus();
 				controller.examineFile(numericInput);
 				return new DefaultInput(controller,"");
 			}
+//  :d                   Delete the current file from the command line list.
 			if (ki.Character == 'd')
 			{
 				//controller.resetStatus();
 				controller.excludeCurrentFile();
+				return new DefaultInput(controller,"");
+			}
+//  :d                   Delete the current file from the command line list.
+			if (ki.Character == 'f')
+			{
+				controller.displayCurrentFileDetails();
 				return new DefaultInput(controller,"");
 			}
 			controller.Alert();
@@ -206,12 +280,176 @@ namespace net.ninebroadcast {
 		public override LessInput beginParse () { 
 			KeyInfo ki = controller.ReadKey();
 
+//  b  ^B  ESC-v      *  Backward one window (or N lines).
+			if (ki.Character == 'v') {
+				//controller.resetStatus();
+				controller.oneWindowBackward(numericInput);
+				return new DefaultInput(controller,"");
+			}
+
+//  ESC-SPACE         *  Forward  one window, but don't stop at end-of-file.
+			if (ki.Character == ' ') {
+				//controller.resetStatus();
+				controller.oneWindowForward(numericInput); // and continue to next document
+				return new DefaultInput(controller,"");
+			}
+
+//  ESC-n             *  Repeat previous search, spanning files.
 			if (ki.Character == 'n') {
 				//controller.resetStatus();
 				controller.findNext("some state variable");
 				return new DefaultInput(controller,"");
 			}
+
+//  ESC-)  RightArrow *  Right one half screen width (or N positions).
+//  ESC-(  LeftArrow  *  Left  one half screen width (or N positions).
+//  ESC-}  ^RightArrow   Right to last column displayed.
+//  ESC-{  ^LeftArrow    Left  to first column.
+//  F                    Forward forever; like "tail -f".
+//  ESC-F                Like F but stop when search pattern is found.
+//  ESC-N             *  Repeat previous search, reverse dir. & spanning files.
+//  ESC-u                Undo (toggle) search highlighting.
+//  g  <  ESC-<       *  Go to first line in file (or line N).
+//  G  >  ESC->       *  Go to last line in file (or line N).
+//  ESC-^F <c1> <c2>  *  Find close bracket <c2>.
+//  ESC-^B <c1> <c2>  *  Find open bracket <c1> 
+//  ESC-M<letter>        Clear a mark.
+
 			return new DefaultInput(controller,"");
 		}
+	}
+// special case for entering command line input
+	public class LineInput : LessInput {
+		private string lineInput;
+		private string linePrefix;
+		public LineInput(LessController lc, string n) {
+			controller = lc;
+			linePrefix = n;
+			lineInput = "";
+			controller.Status(linePrefix);
+		}
+
+		private void moveWordForward()
+		{
+			;  // it's a kind of magic
+		}
+
+		private void moveWordBackward()
+		{
+			;  // it's a kind of magic
+		}
+
+		private void backspace() { ; }
+		private void delete() { ; }
+		private void deleteWord
+		() { ; }
+
+		public override LessInput beginParse () {
+			KeyInfo ki;
+			char key ;
+			int code;
+			int escape=0;
+
+			ControlKeyStates control = ki.ControlKeyState;  // this returns no data
+
+			bool escapeMode = false;
+			int cursorPosition = 0;
+
+
+			ki = controller.ReadKey();
+			key = ki.Character;
+			code = ki.VirtualKeyCode;
+
+			if (key!='\0')
+				code = 0;
+			if (escapeMode)
+			{
+				escapeMode = false;
+				escape = key | code;
+				key = '\0';
+			}
+			if (code == 27)
+				escapeMode = true;
+
+			while (code != 13)
+			{
+//  RightArrow ..................... ESC-l ... Move cursor right one character.                                           
+				if (code == 39 || escape == 'l')
+				{ 
+					if (cursorPosition <= lineInput.Length)
+						cursorPosition++;
+				}
+
+// LeftArrow ...................... ESC-h ... Move cursor left one character.                                            
+				if (code == 37 || escape == 'h')
+				{
+					if (cursorPosition > 0)
+						cursorPosition--;
+				}
+// ctrl-RightArrow  ESC-RightArrow  ESC-w ... Move cursor right one word.
+				if (escape == 39 || escape == 'w') 
+					moveWordForward();  // there is no spoon
+				 
+						// to be stateful or not to be generic that is the question
+
+// ctrl-LeftArrow   ESC-LeftArrow   ESC-b ... Move cursor left one word.                                                 
+				if (escape == 37 || escape == 'b')
+					moveWordBackward();
+						// what she ^^^ said
+// HOME ........................... ESC-0 ... Move cursor to start of line.  
+				if (escape == '0')
+					cursorPosition = 0;
+// END ............................ ESC-$ ... Move cursor to end of line.
+				if (escape == '$')
+					cursorPosition = lineInput.Length;
+// BACKSPACE ................................ Delete char to left of cursor.
+				if (code == 8)
+					backspace();
+// DELETE ......................... ESC-x ... Delete char under cursor.
+				if (code == '~')
+					delete();
+// ctrl-BACKSPACE   ESC-BACKSPACE ........... Delete word to left of cursor.
+				if (escape == 8)
+					deleteWord();
+// ctrl-DELETE .... ESC-DELETE .... ESC-X ... Delete word under cursor.
+				if (escape=='X')
+// ctrl-U ......... ESC (MS-DOS only) ....... Delete entire line.
+				if (escape==27 || code =='u') {
+					cursorPosition = 0;
+					lineInput = "";
+				}
+// UpArrow ........................ ESC-k ... Retrieve previous command line.
+// what... history?
+				if (code==38 || escape=='k') { ; }
+// DownArrow ...................... ESC-j ... Retrieve next command line.
+				if (code==40 || escape=='j') { ; }
+
+// TAB ...................................... Complete filename & cycle.
+				if (code == 9) { ; }
+// SHIFT-TAB ...................... ESC-TAB   Complete filename & reverse cycle.
+				if (escape == 9) { ; }
+// ctrl-L ................................... Complete filename, list all.
+				if (code == 'l') { ; }
+				// display status line and move cursor... 
+				controller.drawStatusPosition(linePrefix,lineInput,cursorPosition);
+
+				ki = controller.ReadKey();
+				key = ki.Character;
+				code = ki.VirtualKeyCode;
+				escape=0;
+				if (key!='\0')
+					code = 0;
+				if (escapeMode)
+				{
+					escapeMode = false;
+					escape = key | code;
+					key = '\0';
+				}
+				if (code == 27)
+					escapeMode = true;
+			}
+			return null;
+		}
+		public string getLine() { return lineInput; }
 	}
 }
